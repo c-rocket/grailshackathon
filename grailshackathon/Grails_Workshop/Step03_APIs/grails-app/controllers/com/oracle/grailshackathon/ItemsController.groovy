7727daecf1cf04213ed66174ec5cdc3a9b737a46
ccrock <cam.crockett.12@gmail.com>
2016-01-19 16:32:15 -0700

    Updating Step 03
    Adding in Step 02

---
+++ grailshackathon/Grails_Workshop/Step03_APIs/grails-app/controllers/com/oracle/grailshackathon/ItemsController.groovy
@@ -0,0 +1,20 @@
+package com.oracle.grailshackathon
+
+import grails.converters.JSON
+
+class ItemsController {
+	def items(){
+		render Item.findAll().collect{
+			[
+				id:it.id,
+				postedBy:it.postedBy,
+				title:it.title,
+				postDate:it.postDate,
+				price:it.price,
+				boughtBy:it.boughtBy,
+				status:it.status,
+				description:it.description,
+			]
+		} as JSON
+	}
+}
