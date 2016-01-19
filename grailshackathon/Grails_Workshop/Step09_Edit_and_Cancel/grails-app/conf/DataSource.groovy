80abe29ebd8436fa1bd13ea3a519db9504dc8ec8
ccrock <cam.crockett.12@gmail.com>
2016-01-19 16:54:19 -0700

    Fixing build

--- grailshackathon/Grails_Workshop/Step09_Edit_and_Cancel/grails-app/conf/DataSource.groovy
+++ grailshackathon/Grails_Workshop/Step09_Edit_and_Cancel/grails-app/conf/DataSource.groovy
@@ -18,40 +18,42 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			dbCreate = "update"
+			pooled = true
+			dialect = org.hibernate.dialect.Oracle10gDialect
+			driverClassName = 'oracle.jdbc.OracleDriver'
+			username = 'devuser' 
+			password = 'Welcome1#'
+			url = 'jdbc:oracle:thin:@129.152.150.206:1521/PDB1.metcsgse00209.oraclecloud.internal'
+			dbCreate = 'validate'
+			properties {
+				validationQuery="select 1 from dual"
+				testWhileIdle=true
+				timeBetweenEvictionRunsMillis=60000
+			}
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			dbCreate = "update"
+			pooled = true
+			dialect = org.hibernate.dialect.Oracle10gDialect
+			driverClassName = 'oracle.jdbc.OracleDriver'
+			username = 'devuser' 
+			password = 'Welcome1#'
+			url = 'jdbc:oracle:thin:@129.152.150.206:1521/PDB1.metcsgse00209.oraclecloud.internal'
+			dbCreate = 'validate'
+			properties {
+				validationQuery="select 1 from dual"
+				testWhileIdle=true
+				timeBetweenEvictionRunsMillis=60000
+			}
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-            }
+			jndiName = "jdbc/hackathonDB"
         }
     }
 }
